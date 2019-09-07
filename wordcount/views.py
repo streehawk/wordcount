62bf12f06d99912421bf2dfc0795c9c0bf8af271
Street Hawk <rayyan.saeed@outlook.com>
2019-09-07 16:04:55 -0700

    First commit for django framework

---
+++ wordcount/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home(request):
+    return render(request, "home.html")
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+
+    worddictionary = {}
+
+    for i in wordlist:
+        if i in worddictionary:
+            worddictionary[i] += 1
+        else:
+            worddictionary[i] = 1
+
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse = True)
+
+    return render(request, "count.html",{'fulltext': fulltext, 'count': len(wordlist), 'sortedwords': sortedwords})
+
+
+def about(request):
+    return render(request, "about.html")
